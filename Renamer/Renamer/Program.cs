c53dc8c2a7604cdbc3ee74411b0d0775f2d5a603
Neonedqe <115537224+Neonedqe@users.noreply.github.com>
2023-05-23 14:26:37 +0200

    Repository hochgeladen

---
+++ Renamer/Renamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
